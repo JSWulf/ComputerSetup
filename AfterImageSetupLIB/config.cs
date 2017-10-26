c58cb7c588a1b52a9d1249378bd4f4b037a85b85
Joseph Wulf <JSW@JSWulf.com>
2017-10-26 15:35:08 -0700

    config files init.

---
+++ AfterImageSetupLIB/config.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AfterImageSetupLIB
+{
+    public class Config
+    {
+        public Config(string file)
+        {
+            ConfigFile = file;
+            try
+            {
+                ReadConfigFile();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        private string ConfigFile { get; set; }
+        public List<string> Items { get;  private set; }
+
+        private void ReadConfigFile()
+        {
+            foreach (var line in File.ReadAllLines(ConfigFile))
+            {
+                if (!line.StartsWith("#"))
+                {
+                    Items.Add(line);
+                }
+            }
+        }
+
+        
+
+
+    }
+    
+}
---
+++ AfterImageSetupLIBTests/ConfigTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AfterImageSetupLIB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AfterImageSetupLIB.Tests
+{
+    [TestClass()]
+    public class ConfigTests
+    {
+        [TestMethod()]
+        public void ConfigTest()
+        {
+            Config printerConf = new Config("Printers.conf");
+
+            foreach (var line in printerConf.Items)
+            {
+                Console.WriteLine(line);
+            }
+            Assert.IsTrue(printerConf.Items.Count == 3);
+        }
+    }
+}
