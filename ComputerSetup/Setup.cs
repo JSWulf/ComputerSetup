918f576983d1fffde89620bd2242dca1c803fcb4
Joseph Wulf <JSW@JSWulf.com>
2018-07-06 15:32:10 -0700

    convisible

--- ComputerSetup/Setup.cs
+++ ComputerSetup/Setup.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
 namespace ComputerSetup
 {
-    class Setup
+    [ComVisible(true)]
+    public class Setup
     {
+        [ComVisible(true)]
+        public void Run()
+        {
+
+        }
     }
 }
